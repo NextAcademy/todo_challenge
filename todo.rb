97dbd653757c57a28fdb0a5aeba5d9a8afb46b87
sammiepls <samantha.lhe@gmail.com>
2017-08-24 12:16:13 +0800

    Add update list method

--- todo.rb
+++ todo.rb
@@ -10,7 +10,6 @@ def add(desc,status)
 end
 
 def list
-
   puts  "No.   Description            Status"
   puts  "===   ===========            ====="
   Task.all.each_with_index do |t,i|
@@ -21,7 +20,18 @@ def list
   end
 end
 
+def update(id,desc,status)
+  if Task.exists?(id)
+    Task.find(id).update(desc:desc,status:status)
+    puts "Task successfully changed! New task:"
+    puts "#{id}. #{desc} #{status}"
+  else
+    puts "Task number #{id} is not found. Are you sure that this is the right task number?"
+  end
+end
+
 case command
 when "add" then add(info[0],info[1])
 when "list" then list
+when "update" then update(info[0],info[1],info[2])
 end
