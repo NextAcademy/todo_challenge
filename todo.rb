30d65bf0f87fa0f47b02c4cb567a5117b5e34258
sammiepls <samantha.lhe@gmail.com>
2017-08-24 13:30:31 +0800

    Add remove method and added capitalization for inputs

--- todo.rb
+++ todo.rb
@@ -2,11 +2,14 @@
 require_relative 'config/application'
 
 # Your Code begins from this line onwards #
+
 command, *info = ARGV
 
 def add(desc,status)
-  Task.create!(desc:desc, status:status)
-  p "Added new task to todo list!"
+  Task.create!(desc:desc.capitalize, status:status.capitalize)
+  puts "Added new task to to-do list!"
+  puts "Description: #{desc.capitalize}"
+  puts "Status: #{status.capitalize}" 
 end
 
 def list
@@ -16,17 +19,29 @@ def list
   id = (i+1).to_s + '.'
   desc = t.desc
   status = t.status
-  puts "#{id.ljust(5)} #{desc.ljust(22)} #{status}"
+  puts "#{id.ljust(5)} #{desc.capitalize.ljust(22)} #{status.capitalize}"
   end
 end
 
 def update(id,desc,status)
-  if Task.exists?(id)
-    Task.find(id).update(desc:desc,status:status)
+  db_id = Task.order(:id).offset(id.to_i-1).limit(1).first.id
+  if Task.exists?(db_id)
+    Task.find(db_id).update(desc:desc,status:status.capitalize)
     puts "Task successfully changed! New task:"
-    puts "#{id}. #{desc} #{status}"
+    puts "Description: #{desc.capitalize}"
+    puts "Status: #{status.capitalize}"
   else
-    puts "Task number #{id} is not found. Are you sure that this is the right task number?"
+    raise "Task number #{id} is not found. Are you sure that this is the right task number?"
+  end
+end
+
+def remove(id)
+  db_id = Task.order(:id).offset(id.to_i-1).limit(1).first.id
+  if Task.exists?(db_id)
+    Task.find(db_id).destroy
+    puts "Task successfully removed!"
+  else
+    raise "Task number #{id} is not found. Are you sure that this is the right task number?"
   end
 end
 
@@ -34,4 +49,6 @@ case command
 when "add" then add(info[0],info[1])
 when "list" then list
 when "update" then update(info[0],info[1],info[2])
+when "remove" then remove(info[0])
+else p "That is not a valid command"
 end
