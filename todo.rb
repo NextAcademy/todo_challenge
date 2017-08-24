4644da6d58e893bb24d6080bebf3c32b41f321a2
sammiepls <samantha.lhe@gmail.com>
2017-08-24 11:23:12 +0800

    Add add task method

--- todo.rb
+++ todo.rb
@@ -2,3 +2,13 @@
 require_relative 'config/application'
 
 # Your Code begins from this line onwards #
+command, *info = ARGV
+
+def add(desc,status)
+  Task.create!(desc:desc, status:status)
+  p "Added new task to todo list!"
+end
+
+case command
+when "add" then add(info[0],info[1])
+end
