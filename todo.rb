5d5851f84d472c5e0fafe564969460281b592e38
qwerqy <amnrsln@gmail.com>
2018-09-04 17:01:37 +0800

    small changes to --list . added new feature --update

--- todo.rb
+++ todo.rb
@@ -10,14 +10,26 @@ if ARGV[0] == '--add'
   p 'add'
   description = ARGV[1]
   status = ARGV[2]
-  Task.create(description: description, status: status)
-  # add method here.
+  task = Task.create(description: description, status: status)
+  task.save
+
 elsif ARGV[0] == '--list'
   p 'list'
-  # list method here.
+  list = Task.all
+  puts "No.         Description                  Status"
+  puts "===         ===========                  ======"
+  list.each do |task|
+  puts "#{task.id}           #{task.description}                    #{task.status}"
+  end
+
 elsif ARGV[0] == '--update'
   p 'update'
   # update method here.
+  no = ARGV[1]
+  description = ARGV[2]
+  status = ARGV[3]
+  task = Task.find_by(id: no)
+  task.update(description: description, status: status)
 elsif ARGV[0] == '--remove'
   p 'remove'
   # remove method here.
