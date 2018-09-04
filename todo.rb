f6e02c8122d85fe0d6a3db3b429204d0d05ec882
qwerqy <amnrsln@gmail.com>
2018-09-04 14:07:54 +0800

    added ARGV arguments, implemented --add feature.

--- todo.rb
+++ todo.rb
@@ -3,3 +3,22 @@ require_relative 'config/application'
 
 # Your Code begins from this line onwards #
 
+# ==========================
+      # ARGV Arguments
+# ==========================
+if ARGV[0] == '--add'
+  p 'add'
+  description = ARGV[1]
+  status = ARGV[2]
+  Task.create(description: description, status: status)
+  # add method here.
+elsif ARGV[0] == '--list'
+  p 'list'
+  # list method here.
+elsif ARGV[0] == '--update'
+  p 'update'
+  # update method here.
+elsif ARGV[0] == '--remove'
+  p 'remove'
+  # remove method here.
+end
