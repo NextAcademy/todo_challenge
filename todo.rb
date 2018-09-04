c609924104bbce89968ac6b9bcfc84c1bfcf222b
qwerqy <amnrsln@gmail.com>
2018-09-04 17:23:49 +0800

    added new gem 'table_print', adjusted --list

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 # Pre-configurations (Change if you want an awesome difficulty assessment)
 require_relative 'config/application'
+require 'table_print'
 
 # Your Code begins from this line onwards #
 
@@ -11,14 +12,10 @@ if ARGV[0] == '--add'
   status = ARGV[2]
   task = Task.create(description: description, status: status)
   task.save
+  puts "Poof! A new task has been born!"
 
 elsif ARGV[0] == '--list'
-  list = Task.all
-  puts "No.         Description                  Status"
-  puts "===         ===========                  ======"
-  list.each do |task|
-  puts "#{task.id}           #{task.description}                    #{task.status}"
-  end
+  tp Task.all
 
 elsif ARGV[0] == '--update'
   no = ARGV[1]
@@ -41,5 +38,4 @@ elsif ARGV[0] == '--remove'
     task = Task.find_by(id: no)
     task.delete
   end
-
 end
